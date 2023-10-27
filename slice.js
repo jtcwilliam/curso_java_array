f5a36a25de3a06cfb39cfd4a945910bf6540ea16
william <jtcwilliam@gmail.com>
2023-10-26 22:38:20 -0300

    enviar arquivo slices

---
+++ slice.js
@@ -0,0 +1,35 @@
+const alunos = ['João',
+    'Juliana',
+    'Ana',
+    'Caio',
+    'Lara',
+    'Marjorie',
+    'Guilherme',
+    'Aline',
+    'Fabiana',
+    'Andre',
+    'Carlos',
+    'Paulo',
+    'Bia',
+    'Vivian',
+    'Isabela',
+    'Vinícius',
+    'Renan',
+    'Renata',
+    'Daisy',
+    'Camilo'];
+
+
+
+const alunos1 = alunos.slice(0, alunos.length / 2);
+
+const alunos2 = alunos.slice(alunos.length / 2);
+const alunos3 = alunos.slice((alunos.length / 2)-1 );
+
+
+
+console.log(alunos1);
+console.log('\n');
+console.log(alunos2);
+console.log('\n');
+console.log(alunos3);
